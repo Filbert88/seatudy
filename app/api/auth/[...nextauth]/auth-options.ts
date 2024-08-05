e38b3749cfd98d4ac05033bcff1e7aa852091255
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-08-05 21:09:34 +0700

    feat: add authentication

--- app/api/auth/[...nextauth]/auth-options.ts
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -1,1 +1,88 @@
-//auth
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import { compare } from "bcrypt";
+import { getServerSession, type AuthOptions } from "next-auth";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 14400,
+  },
+  pages: {
+    signIn: "/auth/signin",
+    error: "/auth/error",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Sign In",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "johndoe@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          throw new Error(
+            JSON.stringify({ message: "Wrong email or password!" })
+          );
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user) {
+          throw new Error(
+            JSON.stringify({ message: "No user found with the entered email!" })
+          );
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!passwordMatch) {
+          throw new Error(JSON.stringify({ message: "Incorrect Password" }));
+        }
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && typeof token.id === "string") {
+        const user = await prisma.user.findUnique({ where: { id: token.id } });
+        if (user) {
+          session.user.id = user.id;
+          session.user.name = user.fullName;
+          session.user.email = user.email;
+          session.user.role = user.role;
+        }
+      }
+      return session;
+    },
+  },
+};
+
+export const getServerAuthSession = () => getServerSession(authOptions);
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,32 @@
-//auth
+import NextAuth from "next-auth";
+import { authOptions } from "./auth-options";
+import nextAuth from "next-auth";
+
+const handler = nextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
+declare module "next-auth" {
+    interface User {
+      id: string; 
+      fullName: string;
+      email: string;
+      role: string; 
+    }
+  
+    interface JWT {
+      id: string;
+      name: string;
+      email: string;
+      role: string;
+    }
+  
+    interface Session {
+      user: {
+        id: string;
+        name: string;
+        email: string;
+        role: string;
+      };
+    }
+  }
