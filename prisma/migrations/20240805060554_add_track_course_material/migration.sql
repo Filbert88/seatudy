43861b56f8a4463d6103c3d8814bcc86bfa02488
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-08-05 13:06:43 +0700

    feat: add track course material on schema

---
+++ prisma/migrations/20240805060554_add_track_course_material/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "CourseMaterialAccess" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "courseMaterialId" TEXT NOT NULL,
+    "accessedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CourseMaterialAccess_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CourseMaterialAccess_userId_courseMaterialId_key" ON "CourseMaterialAccess"("userId", "courseMaterialId");
+
+-- AddForeignKey
+ALTER TABLE "CourseMaterialAccess" ADD CONSTRAINT "CourseMaterialAccess_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CourseMaterialAccess" ADD CONSTRAINT "CourseMaterialAccess_courseMaterialId_fkey" FOREIGN KEY ("courseMaterialId") REFERENCES "CourseMaterial"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
