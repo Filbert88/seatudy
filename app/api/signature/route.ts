663143e64ac50cc6e1f6fc787603b0362f590ec8
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-08-06 18:46:23 +0700

    feat: add upload file functionality

---
+++ app/api/signature/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME!;
+const apiKey = process.env.CLOUDINARY_API_KEY!;
+const apiSecret = process.env.CLOUDINARY_API_SECRET!;
+console.log("name:", cloudName);
+if (!cloudName || !apiKey || !apiSecret) {
+  throw new Error("Cloudinary environment variables are not set");
+}
+
+cloudinary.config({
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
+});
+
+export async function GET(): Promise<NextResponse> {
+  try {
+    const timestamp = Math.round(new Date().getTime() / 1000);
+    const params_to_sign = { timestamp };
+
+    const signature = cloudinary.utils.api_sign_request(
+      params_to_sign,
+      apiSecret
+    );
+
+    return NextResponse.json({ signature, timestamp }, { status: 200 });
+  } catch (error) {
+    console.error('Error generating signature:', error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
