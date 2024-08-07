6389769729c64059b2d639a14dfc0250b253a096
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-08-07 21:07:53 +0700

    feat: update purchase POST api, add POST topup balance api

--- app/api/topup/route.ts
+++ app/api/topup/route.ts
@@ -1,13 +1,17 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options";
 import { PrismaClient, TransactionType } from "@prisma/client";
-import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import { authOptions } from "../auth/[...nextauth]/auth-options";
+import { getServerSession } from "next-auth";
 
 const prisma = new PrismaClient();
 
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 const transactionSchema = z.object({
-  // amount: z.string().min(4, { message: "Amount must be at least 4 digit" }),
+  amount: z.string().min(4, { message: "Amount must be at least 4 digit" }),
   cardNumber: z.string().min(3, { message: "Card number must be at least 3 characters long" }),
   expirationDate: z
     .string()
@@ -17,26 +21,11 @@ const transactionSchema = z.object({
   cardHolderName: z.string().min(3, { message: "Card holder name must be at least 3 characters long" }),
 });
 
-export const POST = async (req: Request, { params }: { params: { id: string } }) => {
-  const courseId = params.id;
+export const POST = async (req: Request) => {
   try {
     const session = await getServerSession({ req, ...authOptions });
     if (!session) {
       return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-    }
-
-    const course = await prisma.course.findUnique({ where: { id: courseId } });
-    if (!course) {
-      return NextResponse.json({ message: "No courses found" }, { status: 404 });
-    }
-
-    const user = await prisma.user.findUnique({ where: { id: session.user.id } });
-    if (!user) {
-      return NextResponse.json({ message: "No users found" }, { status: 404 });
-    }
-
-    if (user.balance < course.price) {
-      return NextResponse.json({ message: "Insufficient balance" }, { status: 400 });
     }
 
     const body = await req.json();
@@ -51,49 +40,27 @@ export const POST = async (req: Request, { params }: { params: { id: string } })
       );
     }
 
-    const { cardNumber, expirationDate, cvc, cardHolderName } = parsedBody.data;
-
-    const alredyEnrolled = await prisma.courseEnrollment.findFirst({
-      where: {
-        userId: session.user.id,
-        courseId: course.id,
-      },
-    });
-    if (alredyEnrolled) {
-      return NextResponse.json({ message: "Already purchased this course" }, { status: 400 });
-    }
+    const { amount, cardNumber, expirationDate, cvc, cardHolderName } = parsedBody.data;
 
     const newData: any = {
       userId: session.user.id,
-      courseId: course.id,
-      amount: course.price,
+      amount,
       cardNumber,
       expirationDate,
       cvc,
       cardHolderName,
-      type: TransactionType.PURCHASE,
+      type: TransactionType.DEPOSIT,
     };
 
     const transaction = await prisma.transaction.create({
       data: newData,
     });
 
-    if (transaction.userId && transaction.courseId) {
-      await prisma.courseEnrollment.create({
-        data: {
-          userId: transaction.userId,
-          courseId: transaction.courseId,
-        },
-      });
-    } else {
-      return NextResponse.json({ message: "Failed to create course enrollment" }, { status: 400 });
-    }
-
     await prisma.user.update({
-      where: { id: transaction.userId },
+      where: { id: session.user.id },
       data: {
         balance: {
-          decrement: transaction.amount,
+          increment: transaction.amount,
         },
       },
     });
