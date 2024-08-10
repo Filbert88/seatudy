9e0f662685728c9de985168854a634cc75092b5a
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-08-11 00:17:57 +0700

    fix: forum get parameter

--- app/api/forum/forum-comment/route.ts
+++ app/api/forum/forum-comment/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(req: NextRequest) {
-  const { postId } = await req.json();
+  const postId = req.nextUrl.searchParams.get("postId");
 
   if (!postId) {
     return NextResponse.json(
