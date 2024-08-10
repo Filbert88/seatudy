00cbf99bcdb5029e13c76388b5ec52f7b854d980
Admantix <williamwijaya499@gmail.com>
2024-08-10 11:16:40 +0700

    chore: Refactor notification page and instructor dashboard components

---
+++ app/instructor-dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar";
+
+const InstructorDashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Instructor Dashboard</h1>
+      </div>
+    </>
+  );
+};
+
+export default InstructorDashboard;
