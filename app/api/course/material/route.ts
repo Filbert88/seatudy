0063eb74f7fc92aff43cfd0e277f034e727afb15
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-08-11 18:17:45 +0700

    feat: add API show material by id, add function to calculate user progressPct

---
+++ app/api/course/material/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
+// Get All Popular Courses
+export const GET = async (req: Request) => {
+  const { searchParams } = new URL(req.url);
+  const courseId = searchParams.get("courseId");
+
+  try {
+    const materials = await prisma.courseMaterial.findMany({
+      where: {
+        courseId: courseId ? courseId : undefined,
+      },
+      include: {
+        accesses: true,
+      },
+    });
+
+    return NextResponse.json({ message: "Success", data: materials }, { status: 200 });
+  } catch (error: any) {
+    console.error("Error in GET /api/course/material", error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+};
