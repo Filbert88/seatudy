a0c7d2c2f22d3b6262c945853bc5559333b52951
Admantix <williamwijaya499@gmail.com>
2024-08-13 14:27:23 +0700

    chore: Add lazy loading for course completion page image

--- app/(user)/course-completion/page.tsx
+++ app/(user)/course-completion/page.tsx
@@ -3,6 +3,8 @@ import { CourseDetailsInterface } from "@/components/types/types";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
+import Image from "next/image";
+import CongratulationImage from "../../../public/assets/congratulations_icon.png";
 
 const CourseCompletionPage = () => {
   const router = useRouter();
@@ -45,8 +47,25 @@ const CourseCompletionPage = () => {
     }
   }, [courseId, router, toast]);
   return (
-    <div>
-      <h1>Course Completion Page</h1>
+    <div className="w-screen h-screen flex justify-center items-center bg-cover bg-primary">
+      <div className="bg-white rounded-lg p-5 flex flex-col font-nunito font-bold text-1xl items-center justify-center max-w-96">
+        <Image
+          src={CongratulationImage}
+          alt="Congratulations"
+          className="w-80 h-auto flex mb-5"
+          objectFit="contain"
+        />
+        <h1 className="flex font-nunito text-1xl text-center mb-2">
+          We know it was hard but you have successfully completed{" "}
+          {courseDetails?.title} Course
+        </h1>
+        <button
+          onClick={() => router.push("/")}
+          className="px-5 py-1 bg-secondary rounded-lg w-80 text-white items-center justify-center hover:cursor-pointer"
+        >
+          Continue
+        </button>
+      </div>
     </div>
   );
 };
