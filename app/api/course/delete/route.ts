b0b9bdb8a7a572af3d7ea7d6c69a49f3de6dc947
Admantix <williamwijaya499@gmail.com>
2024-08-15 23:55:22 +0700

    chore: Delete course and related records

--- app/api/course/delete/route.ts
+++ app/api/course/delete/route.ts
@@ -5,30 +5,65 @@ import { getServerSession } from "next-auth";
 import { Role } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
-  if (req.method !== "POST") {
-    return new NextResponse(`Method ${req.method} Not Allowed`, { status: 405 });
-  }
-
   const session = await getServerSession({ req, ...authOptions });
   if (!session) {
     return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
   }
-
   if (session.user.role !== Role.INSTRUCTOR) {
     return NextResponse.json({ message: "Forbidden" }, { status: 403 });
   }
-
   try {
     const body = await req.json();
     const { courseId } = body;
+    await prisma.courseProgress.deleteMany({
+      where: { enrollment: { courseId } },
+    });
+    await prisma.courseMaterialAccess.deleteMany({
+      where: { courseMaterial: { courseId } },
+    });
+    await prisma.courseMaterial.deleteMany({
+      where: { courseId },
+    });
+    await prisma.assignment.deleteMany({
+      where: { courseId },
+    });
+    await prisma.review.deleteMany({
+      where: { courseId },
+    });
+    await prisma.forumComment.deleteMany({
+      where: {
+        post: {
+          courseId,
+        },
+      },
+    });
+    await prisma.forumPost.deleteMany({
+      where: { courseId },
+    });
+    await prisma.forum.deleteMany({
+      where: { courseId },
+    });
 
+    await prisma.courseCategoryCourse.deleteMany({
+      where: { courseId },
+    });
+
+    await prisma.courseEnrollment.deleteMany({
+      where: { courseId },
+    });
+
+    await prisma.transaction.deleteMany({
+      where: { courseId },
+    });
     const deleteCourse = await prisma.course.delete({
       where: { id: courseId },
     });
-
-    return NextResponse.json({ message: "Success", data: deleteCourse }, { status: 200 });
+    return NextResponse.json(
+      { message: "Success", data: deleteCourse },
+      { status: 200 }
+    );
   } catch (error) {
-    console.error("Error deleting course material:", error);
+    console.error("Error deleting course and related records:", error);
     return NextResponse.json(
       {
         message: "Internal Server Error",
@@ -36,4 +71,4 @@ export async function POST(req: NextRequest) {
       { status: 500 }
     );
   }
-}
+} 
